c0345f1ce0539fa8b546935c26d4cb0cfd23fe89
Deward Seneh <deward.seneh@enhanceit.us>
2020-11-17 15:36:50 -0500

    Task 010

--- Task-011/spark.py
+++ Task-011/spark.py
@@ -2,10 +2,10 @@ from pyspark import SparkContext, SparkConf
 
 if __name__ == "__main__":
     sc = SparkContext("local", "Word Count Task").getOrCreate()
-    f = "/home/user/Desktop/BigData/MindBender_BD/Task-001/Shakespeare.txt"
+    f = "/Desktop/BigData/MindBender_BD/Task-001/Shakespeare.txt"
     words = sc.textFile(f).flatMap(lambda line: line.split(" "))
 
     wordCounts = words.map(lambda word: (
         word, 1)).reduceByKey(lambda a, b: a + b)
     wordCounts.saveAsTextFile(
-        "/home/user/Desktop/BigData/MindBender_BD/Task-010/output")
+        "hdfs://master:9000/wordcount/output")
